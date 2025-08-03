fd51fe4ef5d88171269b8a403164c927382cbf56
nvstre <mbiraut@gmail.com>
2025-08-03 21:52:27 +0200

    final fix

--- src/app/components/navbar-menu.tsx
+++ src/app/components/navbar-menu.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, Transition } from "framer-motion";
 
-const transition = {
+// Tip explicit pentru transition
+const transition: Transition = {
   type: "spring",
   mass: 0.5,
   damping: 11.5,
@@ -41,13 +42,10 @@ export const MenuItem = ({
             <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
               <motion.div
                 transition={transition}
-                layoutId="active" // layoutId ensures smooth animation
+                layoutId="active"
                 className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
               >
-                <motion.div
-                  layout // layout ensures smooth animation
-                  className="w-max h-full p-4"
-                >
+                <motion.div layout className="w-max h-full p-4">
                   {children}
                 </motion.div>
               </motion.div>
@@ -68,7 +66,7 @@ export const Menu = ({
 }) => {
   return (
     <nav
-      onMouseLeave={() => setActive(null)} // resets the state
+      onMouseLeave={() => setActive(null)}
       className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6"
     >
       {children}
@@ -108,6 +106,7 @@ export const ProductItem = ({
   );
 };
 
+// Tip corect în loc de `any`
 export const HoveredLink = ({
   children,
   ...rest
